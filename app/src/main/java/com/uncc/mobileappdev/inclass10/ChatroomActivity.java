e44074ca79a1229eb2f73082db2eb7f2429cddaa
chinmayi5491 <ckulkar2@uncc.edu>
2018-04-09 12:39:25 -0400

    Image click, listview click is done. Onlistview Click its calling intent and going to next page i.e. chatroomActivity.

---
+++ app/src/main/java/com/uncc/mobileappdev/inclass10/ChatroomActivity.java
@@ -0,0 +1,13 @@
+package com.uncc.mobileappdev.inclass10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatroom);
+    }
+}
